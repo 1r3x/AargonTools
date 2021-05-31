2325a517da0c0bf6f610b2c3ca348b47bf7d0cdc
1r3x <akib.hosain@live.com>
2021-06-01 00:23:10 +0600

    just tempDataDbcontext for import the models on develop

--- Models/TempDataDbContext.cs
+++ Models/TempDataDbContext.cs
@@ -17,13 +17,13 @@ namespace AargonTools.Models
         {
         }
 
-        public virtual DbSet<CcPayment> CcPayments { get; set; }
+        public virtual DbSet<DebtorMultiple> DebtorMultiples { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=nv-sqltest01.aai.local;Database=collect;User Id=stephen;Password=Arizona2020!;");
             }
         }
@@ -32,95 +32,17 @@ namespace AargonTools.Models
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
-            modelBuilder.Entity<CcPayment>(entity =>
+            modelBuilder.Entity<DebtorMultiple>(entity =>
             {
-                entity.Property(e => e.ApprovalCode).IsUnicode(false);
+                entity.HasIndex(e => e.DebtorAcct, "x_debtor_acct")
+                    .HasFillFactor((byte)90);
 
-                entity.Property(e => e.ApprovalStatus).IsUnicode(false);
-
-                entity.Property(e => e.BillingAddress1).IsUnicode(false);
-
-                entity.Property(e => e.BillingAddress2).IsUnicode(false);
-
-                entity.Property(e => e.BillingAreaCode).IsUnicode(false);
-
-                entity.Property(e => e.BillingCity).IsUnicode(false);
-
-                entity.Property(e => e.BillingName).IsUnicode(false);
-
-                entity.Property(e => e.BillingPhone).IsUnicode(false);
-
-                entity.Property(e => e.BillingState)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.BillingZip).IsUnicode(false);
-
-                entity.Property(e => e.CardCvv).IsUnicode(false);
-
-                entity.Property(e => e.CardExpMonth)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.CardExpYear)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.CardNum).IsUnicode(false);
-
-                entity.Property(e => e.CardType).IsUnicode(false);
-
-                entity.Property(e => e.CbrFee).HasDefaultValueSql("((0))");
-
-                entity.Property(e => e.Company).IsUnicode(false);
+                entity.HasIndex(e => e.DebtorAcct2, "x_debtor_acct2")
+                    .HasFillFactor((byte)90);
 
                 entity.Property(e => e.DebtorAcct).IsUnicode(false);
 
-                entity.Property(e => e.DebtorAddress1).IsUnicode(false);
-
-                entity.Property(e => e.DebtorAddress2).IsUnicode(false);
-
-                entity.Property(e => e.DebtorAreaCode).IsUnicode(false);
-
-                entity.Property(e => e.DebtorCity).IsUnicode(false);
-
-                entity.Property(e => e.DebtorName).IsUnicode(false);
-
-                entity.Property(e => e.DebtorPhone).IsUnicode(false);
-
-                entity.Property(e => e.DebtorState)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.DebtorZip).IsUnicode(false);
-
-                entity.Property(e => e.ErrorCode).IsUnicode(false);
-
-                entity.Property(e => e.Hsa)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.OrderNumber).IsUnicode(false);
-
-                entity.Property(e => e.RefNumber).IsUnicode(false);
-
-                entity.Property(e => e.Sif)
-                    .IsUnicode(false)
-                    .HasDefaultValueSql("('N')")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.SmsDecline)
-                    .IsUnicode(false)
-                    .HasDefaultValueSql("('N')")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.UserId).IsUnicode(false);
-
-                entity.Property(e => e.UserName).IsUnicode(false);
-
-                entity.Property(e => e.VoidSale)
-                    .IsUnicode(false)
-                    .IsFixedLength(true);
+                entity.Property(e => e.DebtorAcct2).IsUnicode(false);
             });
 
             OnModelCreatingPartial(modelBuilder);
