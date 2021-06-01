9aaae512b01cab44cc6bc6809de964613eaffd00
1r3x <akib.hosain@live.com>
2021-06-02 03:18:01 +0600

    Ado.net connector added

---
+++ Data/ADO/AdoDotNetConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace AargonTools.Data.ADO
+{
+    public class AdoDotNetConnection
+    {
+        private readonly IConfiguration _configuration;
+        public AdoDotNetConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public DataTable GetData(string cmdText)
+        {
+            var objResult = new DataTable();
+            try
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using var myCon = new SqlConnection(connectionString);
+                using var myCommand = new SqlCommand(cmdText, myCon);
+                var da = new SqlDataAdapter(myCommand);
+                da.Fill(objResult);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return objResult;
+
+        }
+    }
+}
