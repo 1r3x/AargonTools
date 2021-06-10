c5bdd15a7d65647b0a680752f046157aefd37722
1r3x <akib.hosain@live.com>
2021-06-11 04:11:33 +0600

    3 controller to one controller

---
+++ Controllers/SetAccountsDetailsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AargonTools.Interfaces;
+using AargonTools.Manager.GenericManager;
+
+namespace AargonTools.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetAccountsDetailsController : ControllerBase
+    {
+        private readonly IAddBadNumbers _contextBadNumbers;
+        private readonly ISetMoveAccount _contextSetMoveAccount;
+        private readonly IAddNotes _contextAddNotes;
+
+        public SetAccountsDetailsController(IAddBadNumbers contextBadNumbers, ISetMoveAccount contextSetMoveAccount, IAddNotes contextAddNotes)
+        {
+            _contextBadNumbers = contextBadNumbers;
+            _contextSetMoveAccount = contextSetMoveAccount;
+            _contextAddNotes = contextAddNotes;
+        }
+
+        [HttpPost("SetBadNumbers/{debtorAcct}&{phoneNo}")]
+        public async Task<IActionResult> SetBadNumbers(string debtorAcct, string phoneNo)
+        {
+            Serilog.Log.Information("  SetBadNumbers => POST");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var data = await _contextBadNumbers.AddBadNumbers(debtorAcct, phoneNo);
+
+                    return Ok(data);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Serilog.Log.Information(e.InnerException, e.Message, e.Data);
+                throw;
+            }
+
+
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+
+        [HttpPost("SetMoveAccount/{debtorAcct}&{toQueue}")]
+        public async Task<IActionResult> SetMoveAccount(string debtorAcct, int toQueue)
+        {
+            Serilog.Log.Information("  SetMoveAccount => POST");
+            try
+            {
+                var data = await _contextSetMoveAccount.SetMoveAccount(debtorAcct, toQueue);
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                Serilog.Log.Information(e.InnerException, e.Message, e.Data);
+                throw;
+            }
+
+        }
+
+
+        [HttpPost("SetNotes/{debtorAcct}&{notes}")]
+        public async Task<IActionResult> SetNotes(string debtorAcct, string notes)
+        {
+            Serilog.Log.Information("  SetNotes => POST");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var data = await _contextAddNotes.CreateNotes(debtorAcct, notes);
+
+                    return Ok(data);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Serilog.Log.Information(e.InnerException, e.Message, e.Data);
+                throw;
+            }
+
+
+            return new JsonResult("Something went wrong") { StatusCode = 500 };
+        }
+
+
+
+
+
+
+
+    }
+}
